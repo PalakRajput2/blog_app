f1b0e4ea369599f055c0831fa1a8e15cc9a436f8
Palak <rpalak317@gmail.com>
2024-07-31 12:30:10 +0530

    adding a new blog

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+// app.js
 const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 8000;
-const mongoose = require("mongoose")
-const userRoute = require("./routes/user");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
 
 // Set the view engine to EJS
 app.set("view engine", "ejs");
@@ -12,17 +16,23 @@ app.set("view engine", "ejs");
 // Set the views directory
 app.set("views", path.resolve("./views"));
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
 
-//Connection of MongoDB 
+// Connection to MongoDB 
 mongoose.connect("mongodb://localhost:27017/blogify")
-.then((e)=>{console.log("Mongo DB connected")});
+  .then(() => { console.log("MongoDB connected") });
 
 app.get("/", (req, res) => {
-  return res.render("home");
+  return res.render("home", {
+    user: req.user,
+  });
 });
 
-app.use("/user",userRoute);
+app.use("/user", userRoute);
+app.use("/blog",blogRoute);
+
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
 });
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const { createHmac }= require("crypto");
 const crypto = require('crypto');
 const {Schema , model} =require("mongoose");
+const { createTokenForUser } = require("../services/authentication");
 const userSchema = new Schema({
     fullName :{
         type : String,
@@ -45,7 +46,7 @@ userSchema.pre('save', function (next){
     next();
 });
 
-userSchema.static("matchPassword", async function(email,password){
+userSchema.static("matchPasswordAndGenerateToken", async function(email,password){
     const user = await this.findOne({email});
     if(!user) throw new Error("User not found");
     
@@ -55,11 +56,11 @@ userSchema.static("matchPassword", async function(email,password){
     .update(password)
     .digest("hex");
 
-    if (hashedPassword !== userProvidedHash)
+    if (hashedPassword !== userProvideHash)
         throw new Error("Incorrect Password");
     
-
-    return user;
+    const token = createTokenForUser(user);
+    return token;
 });
 
 const User = model("user",userSchema);
