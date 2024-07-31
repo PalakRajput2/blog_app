b533697371b8233a0f58c4f817c2082753c9ecab
Palak <rpalak317@gmail.com>
2024-07-31 12:44:33 +0530

    minor change in blog.js in routes

--- routes/blog.js
+++ routes/blog.js
@@ -1,15 +1,16 @@
 const { Router } = require("express");
-
+const Blog = require("../models/blog");
 const router = Router();
 
-router.get("/add-new",(req,res)=>{
-    return res.render("addBlog",{
-        user : req.user,
+router.get("/add-new", (req, res) => {
+    return res.render("addBlog", {
+        user: req.user,
     });
 });
 
-router.post("/",(req,res)=>{
-    return res.render("/");
+router.post("/", (req, res) => {
+    console.log(req.body);
+    return res.redirect("/");
 });
 
 module.exports = router;
